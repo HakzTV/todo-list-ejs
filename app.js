3d9835fb10d60cf6663af026e1326b9068ca6cd9
telvinvarfley <telvinvarfley@gmail.com>
2021-04-18 19:51:13 +0000

    EJS and functionality added

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Defaults for the array
+let items = ["Buy Food","Cook Food","Eat Food"];
+// Engine for rendering EJS
+app.set('view engine' , 'ejs');
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+app.use(express.static("public"))
+
+
+// Home Route
+app.get("/", (req, res)=>{
+
+  let today = new Date();
+
+// Option for date format
+  let options = {
+      weekday: 'long',
+      day: "numeric",
+      month:"long"
+  }
+
+  let day = today.toLocaleDateString("en-US",options)
+// Dynamic data
+  res.render('list', {
+      kindOfDay : day,
+      newListItems : items
+    })
+})
+
+// Handling post requests
+app.post("/", (req,res)=>{
+   item = req.body.newItem;
+    items.push(item)
+    res.redirect("/")
+
+})
+
+//Port listening 
+app.listen(port,()=>{
+    console.log("Server is running at PORt 3000")
+} )
